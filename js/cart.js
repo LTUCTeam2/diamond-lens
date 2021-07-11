eb65b25a92135cb7bc2f18b2a1356fbec0b96188
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-07-11 18:45:23 +0300

    doing feedback page and render all feedbacks from feedback tabel

--- js/cart.js
+++ js/cart.js
@@ -11,14 +11,14 @@ let Cartmaker = function (Name, model, Price) {
 
 Cartmaker.arrayOfObjects = [];
 
-console.table(Cartmaker.arrayOfObjects);
+// console.table(Cartmaker.arrayOfObjects);
 
 // make constructor using array that will be used in the cart every argenamt have there one array
 let arrayOfItemsName = ["sony", "red", "nicon", "canon"];
 let arrayOfItemsModel = ["x", "xx", "xxx", "xxxx"];
 let arrayOfItemsPrice = [100, 200, 300, 400];
 let p = document.querySelectorAll(".myPrag");
-console.log(p);
+// console.log(p);
 // using for loop to make objects
 for (let i = 0; i < arrayOfItemsName.length; i++) {
   new Cartmaker(
@@ -26,18 +26,18 @@ for (let i = 0; i < arrayOfItemsName.length; i++) {
     arrayOfItemsModel[i],
     arrayOfItemsPrice[i]
   );
-  p[i].textContent = `${arrayOfItemsName[i]}`;
-  p[i].setAttribute("class", `${arrayOfItemsName[i]}`);
+  // p[i].textContent = `${arrayOfItemsName[i]}`;
+  // p[i].setAttribute("class", `${arrayOfItemsName[i]}`);
 }
-console.table(Cartmaker.arrayOfObjects);
+// console.table(Cartmaker.arrayOfObjects);
 const spanCartCounter = document.getElementById("cartCounter");
 const myBtn = document.getElementsByTagName("button");
 let i;
-console.log(myBtn);
+// console.log(myBtn);
 for (i = 0; i < myBtn.length; i++) {
   myBtn[i].addEventListener("click", cartCounterFunc);
 }
 
 function cartCounterFunc(e) {
   console.log(e.target.value);
-
+}
---
+++ js/feedback.js
@@ -0,0 +1,165 @@
+"use strict";
+
+let feedbackForm = document.getElementById ('form')
+let webFeedbacks = document.getElementById ('webFeedbacks')
+
+function Formconstructor (firstName, lastName, feedback, country){
+    this.firstName = firstName;
+    this.lastName = lastName; 
+    this.feedback = feedback;
+    this.country = country;
+    Formconstructor.formArray.push(this);
+
+    saveToLs();
+
+}
+
+
+Formconstructor.formArray = [];
+
+
+feedbackForm.addEventListener('submit', submit)
+
+function submit (event){
+    event.preventDefault();
+    // console.log(event);
+
+    const newFirstName = event.target.fname.value;
+    const newLastName = event.target.lname.value; 
+    const newfeedback = event.target.feedback.value;
+    const newCountry = event.target.country.value;  
+
+    new Formconstructor (newFirstName, newLastName, newfeedback, newCountry );
+    // console.log(Formconstructor.formArray);
+
+    // Direct to feedback page
+      location.replace("../pages/feedback.html")
+    
+
+    
+    renderForm();
+    
+} 
+
+
+
+
+
+
+function saveToLs(){
+
+    // we need to convert this array of objects
+    const convertedArr = JSON.stringify(Formconstructor.formArray);
+    
+    localStorage.setItem('FeedbacksLs', convertedArr);
+    
+    
+  }
+
+
+
+  
+
+  function getFromLs(){
+    const data = localStorage.getItem('FeedbacksLs');
+    console.log(data); 
+    
+    const parsedFormArr  = JSON.parse(data); 
+    // console.log(parsedFormArr); // null 
+    if(parsedFormArr){   //parsedFormArr !== null
+  
+        Formconstructor.formArray = parsedFormArr;
+        console.log(Formconstructor.formArray);
+      // [] = null
+      renderForm();
+     
+    }
+  }
+
+  
+
+  // function myFunction() {
+  //   let feedbackCard = document.getElementById ('card')
+  //   let  x = document.createElement("IMG");
+  //   feedbackCard.appendChild(x);
+  //   let divElement= document.createElement('div')
+  //   feedbackCard.appendChild(divElement);
+  //   divElement.setAttribute("class","container")
+  //   let nameElement= document.createElement('h4')
+  //   divElement.appendChild(nameElement);
+  //   nameElement.textContent= ` ${Formconstructor.formArray[0].firstName} ${Formconstructor.formArray[0].lastName}`;
+  //   let countryElement= document.createElement('p')
+  //   divElement.appendChild(countryElement);
+  //   countryElement.textContent= ` Country: ${Formconstructor.formArray[0].country}`;
+  //   let feedbackElement= document.createElement('p')
+  //   divElement.appendChild(feedbackElement);
+  //   feedbackElement.textContent= ` feedback : ${Formconstructor.formArray[0].feedback}`;
+
+
+    
+
+  //   x.setAttribute("src", "https://www.w3schools.com/howto/img_avatar.png");
+  //   x.setAttribute("width", "230");
+  //   x.setAttribute("height", "228");
+  //   x.setAttribute("alt", "card image");
+    
+
+  // }
+  
+
+// let feedbackCard = document.getElementById ('card')
+// let cardImage =document.createElement('img')
+// feedbackCard.appendChild(cardImage)
+// cardImage.src= "https://www.w3schools.com/howto/img_avatar.png"
+
+
+  function renderForm(){
+    // clear all my current uls to prevent duplicate information
+    webFeedbacks.textContent = '';
+   
+    for(let i = 0; i <  Formconstructor.formArray.length; i++){
+
+      // Cards=======================
+      function myFunction() {
+        let feedbackCard = document.getElementById ('card')
+        let everyCardDiv = document.createElement ('div');
+        feedbackCard.appendChild(everyCardDiv);
+        everyCardDiv.setAttribute("class","everyCardDiv")
+        let  x = document.createElement("IMG");
+        everyCardDiv.appendChild(x);
+        let divElement= document.createElement('div')
+        everyCardDiv.appendChild(divElement);
+        divElement.setAttribute("class","container")
+        let nameElement= document.createElement('h4')
+        divElement.appendChild(nameElement);
+        nameElement.textContent= ` ${Formconstructor.formArray[i].firstName} ${Formconstructor.formArray[i].lastName}`;
+        let countryElement= document.createElement('p')
+        divElement.appendChild(countryElement);
+        countryElement.textContent= ` Country: ${Formconstructor.formArray[i].country}`;
+        let feedbackElement= document.createElement('p')
+        divElement.appendChild(feedbackElement);
+        feedbackElement.textContent= ` feedback : ${Formconstructor.formArray[i].feedback}`;
+    
+    
+        
+    
+        x.setAttribute("src", "https://www.w3schools.com/howto/img_avatar.png");
+        x.setAttribute("width", "230");
+        x.setAttribute("height", "228");
+        x.setAttribute("alt", "card image");
+        
+    
+      }
+      myFunction();
+      // Cards=======================
+      // const formLI = document.createElement('li');
+      // webFeedbacks.appendChild(formLI);
+      // formLI.textContent = `-Name: ${Formconstructor.formArray[i].firstName} ${Formconstructor.formArray[i].lastName} -Country: ${Formconstructor.formArray[i].country} -feedback is: ${Formconstructor.formArray[i].feedback} `;
+      
+    }
+    
+  }
+
+  getFromLs();
+
+
