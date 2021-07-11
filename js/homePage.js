87c8cf99bbeb6e3ca058ade5b7c0c1293ed8df7b
aajaber <aajaber.97@gmail.com>
2021-07-11 17:04:44 +0300

    update july 11th

--- js/homePage.js
+++ js/homePage.js
@@ -1,86 +1,132 @@
 'use strict'
 
+
 //=============================== Consts ===============
-const imagesSection = document.getElementById('images-section');
 const leftImage = document.getElementById('left-image');
 const middleImage = document.getElementById('middle-image');
 const rightImage = document.getElementById('right-image');
 
-const contentDesc1= document.getElementById('ContentDesc1');
-const contentTitle1= document.getElementById('ContentTitle1');
+const contentDesc1 = document.getElementById('ContentDesc1');
+const contentTitle1 = document.getElementById('ContentTitle1');
 
-const contentDesc2= document.getElementById('ContentDesc2');
-const contentTitle2= document.getElementById('ContentTitle2');
+const contentDesc2 = document.getElementById('ContentDesc2');
+const contentTitle2 = document.getElementById('ContentTitle2');
 
-const contentDesc3= document.getElementById('ContentDesc3');
-const contentTitle3= document.getElementById('ContentTitle3');
+const contentDesc3 = document.getElementById('ContentDesc3');
+const contentTitle3 = document.getElementById('ContentTitle3');
 
 
 
-let leftImageIndex;
-let rightImageIndex;
-let middleImageIndex;
+let pack1;
+let pack2;
+
+// to do list :
+
+// pack3
+// pack4
+
+// pack5
+// pack6
+
+// pack7
+// pack8
+
+
+
 
 //================================= Constructor =========
-function Content(name, sourcs, description, title) {
-  this.name = name;
-  this.sourcs = sourcs;
-  this.description = description;
-  this.title = title;
+
+function Content(camerName, camerSource, cameraTitle, cameraDescription, lensName, lensSource, lensTitle, lensDescription, micName, micSource, micTitle, micDescription ,price, brand) {
+  this.camerName = camerName;
+  this.camerSource = camerSource;
+  this.cameraTitle = cameraTitle;
+  this.cameraDescription = cameraDescription;
+
+  this.lensName = lensName;
+  this.lensSource = lensSource;
+  this.lensTitle = lensTitle;
+  this.lensDescription = lensDescription;
+
+  this.micName = micName;
+  this.micSource = micSource;
+  this.micTitle = micTitle;
+  this.micDescription = micDescription;
+
+  this.brand=brand;
+  this.price=price;
   Content.contentArray.push(this);
 }
 //========================= Objects Array ===============
 Content.contentArray = [];
 //========================== Created Objects =============
-new Content('canon', '../images/InnerColumn_images/canon.png', 'DSLR', 'EOS Rebel T8i EF-S 18-55mm IS STM Lens Kit');
-new Content('canon2', '../images/InnerColumn_images/canon2.png', 'DSLR', 'EOS RP RF24-105mm F4-7.1 IS STM Lens Kit');
-new Content('mic', '../images/InnerColumn_images/mic.gif', 'Microphone', 'Directional Microphone DM-E1');
-new Content('mic2', '../images/InnerColumn_images/mic2.png', 'Microphone', 'Stereo Microphone DME100');
-new Content('l1', '../images/InnerColumn_images/l1.png', 'Macro', 'RF100mm F2.8 L MACRO IS USM');
-new Content('l2', '../images/InnerColumn_images/l2.png', 'Telephoto Zoom', 'RF70-200mm F4 L IS USM');
+
+new Content('canon', 
+'../images/InnerColumn_images/canon.png', 
+'DSLR', 'EOS Rebel T8i EF-S 18-55mm IS STM Lens Kit',
+  'l1', '../images/InnerColumn_images/l1.png', 
+  'Macro', 'RF100mm F2.8 L MACRO IS USM',
+  'mic', '../images/InnerColumn_images/mic.gif', 
+  'Microphone', 'Directional Microphone DM-E1','canon');
+
+
+new Content('canon2', '../images/InnerColumn_images/canon2.png', 'DSLR', 'EOS RP RF24-105mm F4-7.1 IS STM Lens Kit',
+  'l2', '../images/InnerColumn_images/l2.png', 'Telephoto Zoom', 'RF70-200mm F4 L IS USM',
+  'mic2', '../images/InnerColumn_images/mic2.png', 'Microphone', 'Stereo Microphone DME100', 'canon');
 
 console.log(Content.contentArray);
+
+
 //====================================== Random Number Generator Function====================
 function generateRandomIndexNumber() {
-  return Math.floor(Math.random() * Content.contentArray.length);
+  let rand = Math.floor(Math.random() * Content.contentArray.length);
+  return rand;
 }
-
-//========================== Render Function ================
-
+//========================= Render Function ================
 function renderContent() {
-  while (leftImageIndex === middleImageIndex ||
-    middleImageIndex === rightImageIndex ||
-    leftImageIndex === rightImageIndex ) {
-
-    
-    leftImageIndex = generateRandomIndexNumber();
-    middleImageIndex = generateRandomIndexNumber();
-    rightImageIndex = generateRandomIndexNumber();
-
+  while (pack1 === pack2 ) {
+    pack1 = generateRandomIndexNumber();
+    pack2 = generateRandomIndexNumber();
   }
+//=========================================================================== First Case Canon==================
+  leftImage.src = Content.contentArray[pack1].camerSource;
+  middleImage.src = Content.contentArray[pack1].lensSource;
+  rightImage.src = Content.contentArray[pack1].micSource;
+
+  leftImage.alt = Content.contentArray[pack1].camerName;
+  middleImage.alt = Content.contentArray[pack1].lensName;
+  rightImage.alt = Content.contentArray[pack1].micName;
+
+  contentDesc1.innerHTML = Content.contentArray[pack1].cameraDescription;
+  contentDesc2.innerHTML = Content.contentArray[pack1].lensDescription;
+  contentDesc3.innerHTML = Content.contentArray[pack1].micDescription;
+
+  contentTitle1.innerHTML = Content.contentArray[pack1].cameraTitle;
+  contentTitle2.innerHTML = Content.contentArray[pack1].lensTitle;
+  contentTitle3.innerHTML = Content.contentArray[pack1].micTitle;
+
   
-  leftImage.src = Content.contentArray[leftImageIndex].sourcs;
-  middleImage.src = Content.contentArray[middleImageIndex].sourcs;
-  rightImage.src =Content.contentArray[rightImageIndex].sourcs;
+//=========================================================================== Second Case Canon ==================
+  leftImage.src = Content.contentArray[pack2].camerSource;
+  middleImage.src = Content.contentArray[pack2].lensSource;
+  rightImage.src = Content.contentArray[pack2].micSource;
 
-  leftImage.alt = Content.contentArray[leftImageIndex].name;
-  middleImage.alt = Content.contentArray[middleImageIndex].name;
-  rightImage.alt = Content.contentArray[rightImageIndex].name;
 
-  contentDesc1.innerHTML=Content.contentArray[leftImageIndex].description;
-  contentDesc2.innerHTML=Content.contentArray[middleImageIndex].description;
-  contentDesc3.innerHTML=Content.contentArray[rightImageIndex].description;
+  leftImage.alt = Content.contentArray[pack2].camerName;
+  middleImage.alt = Content.contentArray[pack2].lensName;
+  rightImage.alt = Content.contentArray[pack2].micName;
 
-  contentTitle1.innerHTML=Content.contentArray[leftImageIndex].title;
-  contentTitle2.innerHTML=Content.contentArray[middleImageIndex].title;
-  contentTitle3.innerHTML=Content.contentArray[rightImageIndex].title;
+  contentDesc1.innerHTML = Content.contentArray[pack2].cameraDescription;
+  contentDesc2.innerHTML = Content.contentArray[pack2].lensDescription;
+  contentDesc3.innerHTML = Content.contentArray[pack2].micDescription;
+
+  contentTitle1.innerHTML = Content.contentArray[pack2].cameraTitle;
+  contentTitle2.innerHTML = Content.contentArray[pack2].lensTitle;
+  contentTitle3.innerHTML = Content.contentArray[pack2].micTitle;
 
 }
-
 
 
 //============================================= SlideShow Code================================
-
 var slideIndex = 0;
 showSlides();
 
@@ -104,4 +150,4 @@ function showSlides() {
 
 
 // render function must be called after slider function to prevent a certain issue
-renderContent();
+ renderContent();
