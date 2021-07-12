014fe00e9f9ed95f1c6574fe23eba02c7d3c04b4
haneendarwish <darwishaneen@gmail.com>
2021-07-12 18:48:56 +0300

    add total value to the cart

--- js/cart.js
+++ js/cart.js
@@ -20,6 +20,9 @@ spanCartCounter.textContent = getLocalStorage("inCart");
 
 const constructorFromAccessJsFile = getLocalStorage("acces");
 
+let accessQmultiplyPArray = [];
+let accessQmultiplyP;
+
 let test = [];
 let counter = 0;
 function cartKeysChecker() {
@@ -38,7 +41,11 @@ function cartKeysChecker() {
         test.push(arrayOfLocalStorage[i].model);
         test.push(arrayOfLocalStorage[i].Price);
         test.push(arrayOfLocalStorage[i].inCart);
-        test.push(arrayOfLocalStorage[i].Price * arrayOfLocalStorage[i].inCart);
+
+        accessQmultiplyP = arrayOfLocalStorage[i].Price * arrayOfLocalStorage[i].inCart
+        test.push(accessQmultiplyP);
+        accessQmultiplyPArray.push(accessQmultiplyP);
+
         let section = document.createElement("section");
         section.setAttribute("class", `${arrayOfLocalStorage[i].model}`);
         let hr = document.createElement("hr");
@@ -46,13 +53,13 @@ function cartKeysChecker() {
         main.appendChild(section);
         for (let j = 0; j < test.length; j++) {
           containerForCartPage = document.querySelectorAll(".a");
-          console.log("j", j);
-          console.log("counter", counter);
+          // console.log("j", j);
+          // console.log("counter", counter);
           let a = document.createElement("div");
           a.setAttribute("class", "containerForCartPage a");
           section.appendChild(a);
-          console.log(test[j]);
-          console.log(containerForCartPage);
+          // console.log(test[j]);
+          // console.log(containerForCartPage);
           containerForCartPage[counter].textContent = test[j];
           counter++;
         }
@@ -66,6 +73,9 @@ cartKeysChecker();
 //   test1 = arrayOfLocalStorage.map((x) => x.model);
 //   test2 = arrayOfLocalStorage.map((x) => x.Price);
 
+
+let camQmultiplyPArray = [];
+let camQmultiplyP;
 const cam = getLocalStorage("cam");
 
 if (cam !== null) {
@@ -83,7 +93,11 @@ if (cam !== null) {
       abcArray.push(abc.Model);
       abcArray.push(abc.Price);
       abcArray.push(abc.inCart);
-      abcArray.push(abc.inCart * abc.Price);
+      camQmultiplyP = abc.inCart * abc.Price;
+      abcArray.push(camQmultiplyP);
+      camQmultiplyPArray.push(camQmultiplyP);
+
+      // console.log('total price of cameras products:', camQmultiplyP);
 
       for (let j = 0; j < abcArray.length; j++) {
         containerForCartPage = document.querySelectorAll(".a");
@@ -93,7 +107,7 @@ if (cam !== null) {
         containerForCartPage[counter].textContent = abcArray[j];
         counter++;
       }
-      console.log(i);
+      // console.log(i);
 
       // console.log(abc);
     }
@@ -102,8 +116,52 @@ if (cam !== null) {
     // console.log(getLocalStorage(cam[i].Model) !== null);
   }
 }
+console.log('Camera Total Array', camQmultiplyPArray);
 
+
+camQmultiplyP = camQmultiplyPArray.reduce(add,0); 
+accessQmultiplyP = accessQmultiplyPArray.reduce(add2,0); 
+let sumCamandAccess = camQmultiplyP + accessQmultiplyP;
+
+
+function add(accumulator, a) {
+  return accumulator + a;
+}
+// console.log('camera sum', camQmultiplyP);
+
+function add2(accumulator2, a2) {
+  return accumulator2 + a2;
+}
+
+// console.log('Accessories sum', accessQmultiplyP);
+console.log('Total Sum', sumCamandAccess);
+
+
+function totalSection() {
+  let sectionForTotal = document.createElement('section');
+  sectionForTotal.setAttribute('id','sectionForTotal');
+  main.appendChild(sectionForTotal);
+  sectionForTotal.textContent = `Total Price: ${sumCamandAccess}`;
+}
+
+totalSection();
+
+
+
+// let totalPriceCamAndAccess = 0;
+// for (let i = 0; i < camQmultiplyPArray.length; i++) {
+//     totalPriceCamAndAccess += camQmultiplyPArray[i];
+//         console.log(camQmultiplyPArray);
+//   }
+
+// console.log(totalPriceCamAndAccess);
 // let abc = getLocalStorage('Canon EOS 6D');
 
 // console.log(containerForCartPage);
 // console.log(typeof(containerForCartPage));
+
+// totalPriceCamAndAccess = camQmultiplyP + accessTotal;
+
+
+
+
