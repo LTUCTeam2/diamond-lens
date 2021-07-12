3251741db112af579d2f4d3f54cda1e462c914ca
Ahmad Jallad <ahmadjlad@hotmail.com>
2021-07-12 16:37:57 +0300

    add button to delete all cart

--- js/cart.js
+++ js/cart.js
@@ -19,6 +19,22 @@ const main = document.querySelector("#main");
 spanCartCounter.textContent = getLocalStorage("inCart");
 
 const constructorFromAccessJsFile = getLocalStorage("acces");
+// ======== remove button
+function remove(id, model) {
+  let deleteButton = document.createElement("Button");
+  deleteButton.setAttribute("class", id);
+  let sec = document.querySelector(`.${id}`);
+  sec.appendChild(deleteButton);
+  deleteButton.textContent = "X";
+  deleteButton.addEventListener("click", () => {
+    // main.removeChild(sec);
+    sec.remove();
+    localStorage.removeItem(model);
+    // sec.style.display = "none";
+  });
+}
+//  end of remove button
+// ======== first function for accsse page
 
 let test = [];
 let counter = 0;
@@ -44,15 +60,14 @@ function cartKeysChecker() {
         let hr = document.createElement("hr");
         main.appendChild(hr);
         main.appendChild(section);
+        remove(`${arrayOfLocalStorage[i].model}`, arrayOfLocalStorage[i].Name);
+
         for (let j = 0; j < test.length; j++) {
-          containerForCartPage = document.querySelectorAll(".a");
-          console.log("j", j);
-          console.log("counter", counter);
           let a = document.createElement("div");
           a.setAttribute("class", "containerForCartPage a");
           section.appendChild(a);
-          console.log(test[j]);
-          console.log(containerForCartPage);
+          containerForCartPage = document.querySelectorAll(".a");
+          console.log(containerForCartPage[counter]);
           containerForCartPage[counter].textContent = test[j];
           counter++;
         }
@@ -70,13 +85,7 @@ const cam = getLocalStorage("cam");
 
 if (cam !== null) {
   for (let i = 0; i < cam.length; i++) {
-
     if (getLocalStorage(cam[i].Model) !== null) {
-      let camSection = document.createElement('section');
-      let hr = document.createElement('hr');
-      main.appendChild(hr);
-      main.appendChild(camSection);
-
       let abc = getLocalStorage(cam[i].Model);
       let abcArray = [];
       abcArray.push(abc.Name);
@@ -85,11 +94,17 @@ if (cam !== null) {
       abcArray.push(abc.inCart);
       abcArray.push(abc.inCart * abc.Price);
 
+      let camSection = document.createElement("section");
+      camSection.setAttribute("class", `${cam[i].Name}`);
+      let hr = document.createElement("hr");
+      main.appendChild(hr);
+      main.appendChild(camSection);
+      remove(`${cam[i].Name}`, cam[i].Model);
       for (let j = 0; j < abcArray.length; j++) {
-        containerForCartPage = document.querySelectorAll(".a");
-        let camDiv = document.createElement('div');
-        camDiv.setAttribute('class', 'containerForCartPage a');
+        let camDiv = document.createElement("div");
+        camDiv.setAttribute("class", "containerForCartPage a");
         camSection.appendChild(camDiv);
+        containerForCartPage = document.querySelectorAll(".a");
         containerForCartPage[counter].textContent = abcArray[j];
         counter++;
       }
